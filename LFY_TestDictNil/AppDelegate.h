feae34e2de52eeb3e7beed884cde5cc0848bd1d1
Zhe Song <zsong@mobileinteractive.com>
2017-03-23 09:46:40 +0800

    Initial Commit

---
+++ LFY_TestDictNil/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LFY_TestDictNil
+//
+//  Created by administrator on 2017/3/23.
+//  Copyright © 2017年 Dareway Software Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LFY_TestDictNil/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LFY_TestDictNil
+//
+//  Created by administrator on 2017/3/23.
+//  Copyright © 2017年 Dareway Software Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
